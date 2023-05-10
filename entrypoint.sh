d43f88bc0092f3a3cdb93b2423886102c3ad9652
Ivica <ivica-matic@outlook.com>
2023-05-10 15:21:51 +0100

    Removed migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,27 @@ set -e
 #wget -O /tmp/pgstac.sql https://raw.githubusercontent.com/stac-utils/pgstac/main/src/pgstac/migrations/pgstac.0.7.1.sql
 #
 ## Connect to the database and initialize it with the schema
-#PGPASSWORD="$POSTGRES_PASS" psql -h "$POSTGRES_HOST_WRITER" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DBNAME" -f /tmp/pgstac.sql
 ## Clean up the temporary schema file
 #rm /tmp/pgstac.sql
+echo "CREATE EXTENSION IF NOT EXISTS postgis;" > /tmp/install_postgis_extension.sql
+PGPASSWORD="$POSTGRES_PASS" psql -h "$POSTGRES_HOST_WRITER" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DBNAME" -f /tmp/install_postgis_extension.sql
 
+cat << EOF > setup.py
+import os
 
-FLASK_APP=manage.py python3 manage.py db migrate
-FLASK_APP=manage.py python3 manage.py db upgrade
+from flask_cli import FlaskGroup
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from app import blueprint
+from app.main import create_app, db
+db.create_all()
+EOF
+
+FLASK_APP=manage.py python3 setup.py
+
+# FLASK_APP=manage.py python3 manage.py db migrate
+# FLASK_APP=manage.py python3 manage.py db upgrade
 
 echo "PostgreSQL database initialization complete."
 exec "$@"
