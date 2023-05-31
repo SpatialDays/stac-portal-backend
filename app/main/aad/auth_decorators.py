8f7c9ff0bd290c49c8eb109303fd33f0807798c6
Ivica Matić <ivica-matic@outlook.com>
2023-05-31 16:26:27 +0100

    Update auth_decorators.py

--- app/main/aad/auth_decorators.py
+++ app/main/aad/auth_decorators.py
@@ -61,13 +61,13 @@ class AuthDecorator:
                     
                     # Check if the user's role is allowed
                     # could we do: 
-                    authenticated = any(role in allowed_roles for role in roles)
-                    # authenticated = False
-                    # for i in roles:
-                    #     for j in allowed_roles:
-                    #         if i == j:
-                    #             authenticated = True
-                    #             break
+                    # authenticated = any(role in allowed_roles for role in roles)
+                    authenticated = False
+                    for i in roles:
+                        for j in allowed_roles:
+                            if i == j:
+                                authenticated = True
+                                break
                     
                     if not authenticated:
                         # Return a 401 Unauthorized response
