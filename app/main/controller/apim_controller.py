9b8380d8871cdf1b7c66ffa3d08695fb9fccf540
Ivica <ivica-matic@outlook.com>
2023-06-02 09:59:42 +0100

    feat: added endpoint for getting the token

--- app/main/controller/apim_controller.py
+++ app/main/controller/apim_controller.py
@@ -16,9 +16,27 @@ class GetAPIMToken(Resource):
     @auth_decorator.header_decorator(
         allowed_roles=["StacPortal.Viewer", "StacPortal.Creator"]
     )
-    def get(self,oid:str, preffered_username:str, name:str):
-        apim_token = f"{oid} apim_token"
-        return {"apim_token": apim_token}, 200
+    def get(self, oid: str, preferred_username: str, name: str):
+        try:
+            secrets = get_subscription_secrets_for_user(oid)
+        except APIMSubscriptionNotFoundError:
+            try:
+                get_user_from_apim(oid)
+            except APIMUserNotFoundError:
+                first_name = name.split(" ")[0]
+                last_name = "".join(name.split(" ")[1:])
+                try:
+                    create_user_on_apim(oid, preferred_username, first_name, last_name)
+                except APIMUserCreationError:
+                    return {"message": "Error creating user on APIM"}, 500
+
+            try:
+                make_subscription_for_user_on_all_apis(oid)
+                secrets = get_subscription_secrets_for_user(oid)
+            except APIMSubscriptionCreationError:
+                return {"message": "Error creating subscription on APIM"}, 500
+
+        return {"secrets": secrets}, 200
 
 
 @api.route("/refresh/")
