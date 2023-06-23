28b2943b022e1c43f6e4b035b6279b57f48fe649
Ivica <ivica-matic@outlook.com>
2023-06-23 14:04:14 +0100

    refactor: moved item related code into stac module

--- app/main/controller/stac_controller.py
+++ app/main/controller/stac_controller.py
@@ -3,6 +3,7 @@ from flask_restx import Resource
 from ..aad.auth_decorators import AuthDecorator
 from ..service.stac_service import *
 from ..util.dto import StacDto
+from flask import request
 
 auth_decorator = AuthDecorator()
 
@@ -10,7 +11,7 @@ api = StacDto.api
 
 
 @api.route("/")
-class CollectionsList(Resource):
+class ListOfCollections(Resource):
     @api.doc(description="List all collections on the stac-api server")
     @api.response(200, "Success")
     @auth_decorator.header_decorator(
@@ -22,7 +23,7 @@ class CollectionsList(Resource):
 
 @api.route("/<collection_id>/")
 class Collection(Resource):
-    @api.doc(description="get_collection")
+    @api.doc(description="Get specific collection by ID")
     @api.response(200, "Success")
     @api.response(404, "Collection not found")
     @auth_decorator.header_decorator(
@@ -40,7 +41,7 @@ class Collection(Resource):
 @api.route("/<collection_id>/items/")
 class CollectionItems(Resource):
 
-    @api.doc(description="get_collection_items")
+    @api.doc(description="Get all items from collection")
     @api.response(200, "Success")
     @api.response(404, "Collection not found")
     @auth_decorator.header_decorator(
@@ -54,11 +55,36 @@ class CollectionItems(Resource):
                        "message": "Collection with this ID not found",
                    }, 404
 
+    @api.doc(description="Add item to collection")
+    @api.response(200, "Success")
+    @api.response(404, "Collection not found")
+    @api.response(409, "Item already exists")
+    @api.response(400, "Invalid payload")
+    @api.expect(StacDto.item_dto, validate=True)
+    @auth_decorator.header_decorator(
+        allowed_roles=["StacPortal.Creator"]
+    )
+    def post(self, collection_id: str):
+        try:
+            return add_stac_item(collection_id, request.json)
+        except CollectionDoesNotExistError:
+            return {
+                       "message": "Collection with this ID not found",
+                   }, 404
+        except ItemAlreadyExistsError:
+            return {
+                       "message": "Item with this ID already exists",
+                   }, 409
+        except InvalidCollectionPayloadError:
+            return {
+                       "message": "Invalid payload",
+                   }, 400
+
 
 @api.route("/<collection_id>/items/<item_id>/")
 class CollectionItem(Resource):
 
-    @api.doc(description="get_collection_item")
+    @api.doc(description="Get specific item from specific collection")
     @api.response(200, "Success")
     @api.response(404, "Collection not found")
     @api.response(404, "Item not found")
@@ -76,4 +102,46 @@ class CollectionItem(Resource):
         except CollectionDoesNotExistError:
             return {
                        "message": "Collection with this ID not found",
+                   }, 404
+
+
+@api.route("/collections/<collection_id>/items/<item_id>/")
+class CollectionItem(Resource):
+
+    @api.doc(description="Update specific item in a specific collection")
+    @api.response(200, "Success")
+    @api.response(403, "Unauthorized.")
+    @api.expect(StacDto.item_dto, validate=True)
+    @auth_decorator.header_decorator(
+        allowed_roles=["StacPortal.Creator"]
+    )
+    def put(self, collection_id: str, item_id: str):
+        try:
+            return update_stac_item(collection_id, item_id, request.json), 200
+        except CollectionDoesNotExistError:
+            return {
+                       "message": "Collection with this ID not found",
+                   }, 404
+        except ItemDoesNotExistError:
+            return {
+                       "message": "Item with this ID not found",
+                   }, 404
+
+    @api.doc(description="Remove specific item from specific collection")
+    @api.response(200, "Success")
+    @api.response(403, "Unauthorized.")
+    @api.response("4xx", "Stac API reported error")
+    @auth_decorator.header_decorator(
+        allowed_roles=["StacPortal.Creator"]
+    )
+    def delete(self, collection_id: str, item_id: str):
+        try:
+            return remove_stac_item(collection_id, item_id), 200
+        except CollectionDoesNotExistError:
+            return {
+                       "message": "Collection with this ID not found",
+                   }, 404
+        except ItemDoesNotExistError:
+            return {
+                       "message": "Item with this ID not found",
                    }, 404
