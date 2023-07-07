100041066ec8ccd9bb98151eebcf1104d5478da3
Ivica <ivica-matic@outlook.com>
2023-07-07 11:31:23 +0100

    cicd: fixed pywsgi.py old

--- pywsgi.py
+++ pywsgi.py
@@ -1,9 +1,11 @@
+# DO NOT OPTIMISE IMPORTS IN THIS FILE
+
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 
-from manage import app
 monkey.patch_all()
 
+from manage import app
 
 http_server = WSGIServer(('0.0.0.0', 5001), app)
 http_server.serve_forever()
